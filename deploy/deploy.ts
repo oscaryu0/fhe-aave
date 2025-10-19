2ec53aa03d4cddceddf8bbf5e9f1f224cf273580
oscaryu0 <exxhsbw6813@hotmail.com>
2025-10-19 14:49:59 +0800

    deploy script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,20 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedToken = await deploy("fheUSDT", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const deployedLendingPool = await deploy("FHELendingPool", {
+    from: deployer,
+    args: [deployedToken.address],
+    log: true,
+  });
+
+  console.log(`fheUSDT contract: ${deployedToken.address}`);
+  console.log(`FHELendingPool contract: ${deployedLendingPool.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_lending"; // id required to prevent reexecution
+func.tags = ["Lending"];
